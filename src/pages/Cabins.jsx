d4c759606416da856bb7d4d841288d94234f08f8
Diogo Coelho <diogocoelho19988@gmail.com>
2024-10-20 16:33:50 +0100

    supabase project created and connected with the front-end

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 export default function Cabins() {
+  useEffect(() => {
+    const fetchedCabins = async () => {
+      const cabins = await getCabins();
+      console.log(cabins);
+    };
+
+    fetchedCabins();
+  }, []);
+
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
---
+++ src/services/apiCabins.js
@@ -0,0 +1,13 @@
+import supabase from "./supabase.js";
+
+// Querying supabase client to get all Cabins
+export async function getCabins() {
+  const { data, error } = await supabase.from("cabins").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+}
